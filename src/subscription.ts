9198628f495b7bfa22effcddebf6600c429082ce
Jones <153377475+Ranshen1209@users.noreply.github.com>
2026-03-14 11:02:35 +0800

    feat: add personal subscription apps

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 6489287752973274,
+  name: 'RanshenのGDK订阅',
+  version: 1,
+  author: 'ranshen',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/Ranshen1209/GKD-subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
